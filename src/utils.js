26d4b5b9393655a930d31ae59271d7140bb5447e
eneiluj <eneiluj@users.noreply.github.com>
2020-10-29 23:23:36 +0000

    deploy: 6222e227f359d7aa3dad996c8dcb549d63ab7a65

---
+++ src/utils.js
@@ -0,0 +1,51 @@
+let mytimer = 0
+export function delay(callback, ms) {
+	return function() {
+		const context = this
+		const args = arguments
+		clearTimeout(mytimer)
+		mytimer = setTimeout(function() {
+			callback.apply(context, args)
+		}, ms || 0)
+	}
+}
+
+export function addCustomEventListener(selector, event, handler) {
+	const rootElement = document.querySelector('body')
+	rootElement.addEventListener(event, function(evt) {
+		let targetElement = evt.target
+		while (targetElement != null) {
+			if (targetElement.matches(selector)) {
+				handler(evt, targetElement)
+				return
+			}
+			targetElement = targetElement.parentElement
+		}
+	},
+	true)
+}
+
+export function humanFileSize(bytes, approx = false, si = false, dp = 1) {
+	const thresh = si ? 1000 : 1024
+
+	if (Math.abs(bytes) < thresh) {
+		return bytes + ' B'
+	}
+
+	const units = si
+		? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+		: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+	let u = -1
+	const r = 10 ** dp
+
+	do {
+		bytes /= thresh
+		++u
+	} while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1)
+
+	if (approx) {
+		return Math.floor(bytes) + ' ' + units[u]
+	} else {
+		return bytes.toFixed(dp) + ' ' + units[u]
+	}
+}
