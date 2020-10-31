e749e4996a72e63892c0cfd4c6a3235e85311b67
Julien Veyssier <eneiluj@posteo.net>
2020-10-31 02:55:07 +0100

    begin to adapt no-vue wrapper to button switches
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/filePickerWrapper.js
+++ src/filePickerWrapper.js
@@ -18,7 +18,7 @@ import NcFilePicker from './components/NcFilePicker'
 // eslint-disable-next-line
 'use strict'
 
-window.createFilePicker = function(mp, url, login, password) {
+window.createFilePicker = function(mp, url, login, password, enableGetFilesPath = true) {
 	const vm = new Vue({
 		el: '#' + mp,
 		render: h => h(NcFilePicker, {
@@ -26,6 +26,7 @@ window.createFilePicker = function(mp, url, login, password) {
 				ncUrl: url,
 				ncLogin: login,
 				ncPassword: password,
+				enableGetFilesPath,
 			},
 		}),
 	})
