df7e9dd673e9da14f3c802185b569a437da05d88
Julien Veyssier <eneiluj@posteo.net>
2020-11-23 18:39:38 +0100

    add script to generate static translations JS file
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/translations.js
+++ src/translations.js
@@ -1,10 +1,11 @@
 export const translations = {
 	fr: `msgid ""
-	msgstr ""
-	"Last-Translator: Translator, 2020\n"
-	"Content-Type: text/plain; charset=UTF-8\n"
-	"Language: sv\n"
-	"Plural-Forms: nplurals=2; plural=(n != 1);\n"
-	msgid "abc"
-	msgstr "def"`,
+msgstr ""
+"Last-Translator: Translator, 2020\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Language: sv\n"
+"Plural-Forms: nplurals=2; plural=(n != 1);\n"
+msgid "abc"
+msgstr "defYéyé"
+`,
 }
