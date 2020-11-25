60069e96d7710fd21f8af1dec6ffcbf31a069a68
Julien Veyssier <eneiluj@posteo.net>
2020-11-25 18:50:21 +0100

    speedup translation update
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -2,7 +2,7 @@
 
 APP_NAME=nextcloud-webdav-filepicker
 rm -rf /tmp/$APP_NAME
-git clone https://github.com/eneiluj/$APP_NAME /tmp/$APP_NAME -b l10n_master
+git clone https://github.com/eneiluj/$APP_NAME /tmp/$APP_NAME -b l10n_master --single-branch
 cp -r /tmp/$APP_NAME/translationfiles/[a-z][a-z]_[A-Z][A-Z] ../translationfiles/
 rm -rf /tmp/$APP_NAME
 
