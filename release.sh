9c3c6da6bd9e6cded8900b09f2a2598573b3fe41
Julien Veyssier <eneiluj@posteo.net>
2021-01-19 14:13:16 +0100

    fix release script
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NPM_TOKEN=`cat ~/.nextcloud/secrets/NPM_TOKEN | tr -d '\n'`
+export NPM_TOKEN=`cat ~/.nextcloud/secrets/NPM_TOKEN | tr -d '\n'`
 npm ci
 npm run build
-npm publish
+NPM_CONFIG_TOKEN=$NPM_TOKEN npm publish
