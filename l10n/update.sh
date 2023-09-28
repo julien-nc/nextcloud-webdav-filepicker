9fed096917460670906750d7e3c1457e2be13163
Julien Veyssier <julien-nc@posteo.net>
2023-09-28 10:24:04 +0200

    adjust l10n script
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -2,7 +2,7 @@
 
 APP_NAME=nextcloud-webdav-filepicker
 rm -rf /tmp/$APP_NAME
-git clone https://github.com/julien-nc/$APP_NAME /tmp/$APP_NAME -b l10n_master --single-branch
+git clone https://github.com/julien-nc/$APP_NAME /tmp/$APP_NAME -b l10n_main --single-branch
 cp -r /tmp/$APP_NAME/translationfiles/[a-z][a-z]_[A-Z][A-Z] ../translationfiles/
 rm -rf /tmp/$APP_NAME
 
